83441e58d892ac180865b3d6c2eefc182249e115
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2026-02-09 17:46:50 -0800

    add touch to ensure local claude.json exists for mount

--- tools/claude-docker/clauded.sh
+++ tools/claude-docker/clauded.sh
@@ -2,11 +2,18 @@
 
 IMAGE="claude-docker:latest"
 CONF_LOCAL="$HOME/.config/claude-docker"
+CONFDIR_CLAUDE="$CONF_LOCAL/.claude"
+
+# ensure the local config dir exists (NOTE: this is separate from the hosts's real configs)
 mkdir -p "$CONF_LOCAL"
+
+# make sure the local .claude.json exists; if it didn't already then this will ensure the session
+# and settings are correctly preserved for the next run.
+touch $CONF_LOCAL/.claude.json
 
 docker run \
     -v $PWD:/workspace \
-    -v $CONF_LOCAL/.claude:/home/ubuntu/.claude \
-    -v $CONF_LOCAL/.claude.json:/home/ubuntu/.claude.json \
+    -v "$CONFDIR_CLAUDE":/home/ubuntu/.claude \
+    -v "$CONF_LOCAL/.claude.json":/home/ubuntu/.claude.json \
     -it "$IMAGE"
 
